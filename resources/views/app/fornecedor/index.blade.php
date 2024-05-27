f44e7012dc7fa79269cc49386c4463d5fae93f6c
Isaac N Reis <isaac.reis@teknisa.com>
2024-05-27 19:46:33 -0300

    first commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,10 @@
+<h1>Fornocedor</h1>
+
+{{-- @if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif (count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Não existem fornecedores cadastrados</h3>
+@endif --}}
+
